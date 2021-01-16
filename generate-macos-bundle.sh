8e568098c87f2af796cd93f02d685b7b9efba59d
Nico Just <mail@njust.de>
2021-01-16 21:45:45 +0100

    MacOS build

--- generate-macos-bundle.sh
+++ generate-macos-bundle.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 mv ./target/release/bundle/osx/ktail.app/Contents/MacOS/ktail ./target/release/bundle/osx/ktail.app/Contents/MacOS/ktail-bin
+chmod +x target/release/bundle/osx/ktail.app/Contents/MacOS/ktail-bin
+
 cp ./assets/MacOS/ktail ./target/release/bundle/osx/ktail.app/Contents/MacOS/
+chmod +x target/release/bundle/osx/ktail.app/Contents/MacOS/ktail
+
 cp ./assets/MacOS/gdk-pixbuf-query-loaders ./target/release/bundle/osx/ktail.app/Contents/MacOS/
+chmod +x ./target/release/bundle/osx/ktail.app/Contents/MacOS/gdk-pixbuf-query-loaders
 
 cp -R ./assets/MacOS/Resources ./target/release/bundle/osx/ktail.app/Contents/
 cp -R ./assets/MacOS/lib ./target/release/bundle/osx/ktail.app/Contents/MacOS/
