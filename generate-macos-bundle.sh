1499a6d28db9e0eafc148160a673594be3c7904d
Nico Just <mail@njust.de>
2021-01-22 17:50:21 +0100

    Removed duplicate files

--- generate-macos-bundle.sh
+++ generate-macos-bundle.sh
@@ -10,6 +10,9 @@ cp ./assets/MacOS/gdk-pixbuf-query-loaders ./target/release/bundle/osx/ktail.app
 chmod +x ./target/release/bundle/osx/ktail.app/Contents/MacOS/gdk-pixbuf-query-loaders
 
 cp -R ./assets/MacOS/Resources ./target/release/bundle/osx/ktail.app/Contents/
+cp -R ./assets/icons ./target/release/bundle/osx/ktail.app/Contents/Resources
+cp -R ./assets/themes/Mint-Y-Grey ./target/release/bundle/osx/ktail.app/Contents/Resources/themes
+
 cp -R ./assets/MacOS/lib ./target/release/bundle/osx/ktail.app/Contents/MacOS/
 
 cd ./target/release/bundle/osx/
