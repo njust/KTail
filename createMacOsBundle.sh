991debd27f324d41be9a6a2ab3cc1d88ee6ca3bd
Nico Just <mail@njust.de>
2022-01-19 15:08:10 +0100

    Added missing lib for macos build

--- createMacOsBundle.sh
+++ createMacOsBundle.sh
@@ -73,6 +73,7 @@ cp "$LIB_SRC/libXdmcp.6.dylib" "$LIB_DIR"
 cp "$LIB_SRC/libjpeg.9.dylib" "$LIB_DIR"
 cp "$LIB_SRC/liblzo2.2.dylib" "$LIB_DIR"
 cp "/usr/local/Cellar/openssl@1.1/1.1.1m/lib/libssl.1.1.dylib" "$LIB_DIR"
+cp "/usr/local/Cellar/openssl@1.1/1.1.1m/lib/libcrypto.1.1.dylib" "$LIB_DIR"
 
 mkdir "$LIB_DIR/gdk-pixbuf-2.0"
 cp -R -L /usr/local/lib/gdk-pixbuf-2.0/2.10.0/ "$LIB_DIR/gdk-pixbuf-2.0"
